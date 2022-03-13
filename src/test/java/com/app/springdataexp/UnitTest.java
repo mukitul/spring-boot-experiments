f1886a5b058ecc355769761f0db5629e2deacbb9
mukitul.islam <mukitul.islam@reddotdigitalit.com>
2022-03-13 13:16:21 +0600

    add test for year calculation

--- src/test/java/com/app/springdataexp/UnitTest.java
+++ src/test/java/com/app/springdataexp/UnitTest.java
@@ -1,11 +1,9 @@
 package com.app.springdataexp;
 
 import com.app.springdataexp.cornexp.CornExpService;
-import com.app.springdataexp.csv.CSVService;
 import com.app.springdataexp.csv.CSVRecordDto;
+import com.app.springdataexp.csv.CSVService;
 import com.app.springdataexp.enumexp.CountryCode;
-import com.app.springdataexp.enumexp.SpeStudentType;
-import com.app.springdataexp.enumexp.StudentType;
 import com.app.springdataexp.listexp.StudentService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -59,5 +62,21 @@ public class UnitTest {
     @Test
     public void getAllStdTest() {
         studentService.showAllStudent();
+    }
+
+    @Test
+    public void dateTest() {
+        String sDate1 = "31/12/1998";
+        try {
+            Date dob = new SimpleDateFormat("dd/MM/yyyy").parse(sDate1);
+            LocalDate dobLD = dob.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+            LocalDate ld = new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+            Period difference = Period.between(dobLD, ld);
+            System.out.println(difference.getYears());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
