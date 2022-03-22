af5da13679597509c6785386fb807c8a10249ef5
mukitul.islam <mukitul.islam@reddotdigitalit.com>
2022-03-22 11:06:23 +0600

    Add date test

--- src/test/java/com/app/springdataexp/UnitTest.java
+++ src/test/java/com/app/springdataexp/UnitTest.java
@@ -69,7 +69,7 @@ public class UnitTest {
 
     @Test
     public void dateTest() {
-        //dateService.calculateDifferenceBetweenGivenDateAndCurrentDate();
+        dateService.calculateDifferenceBetweenGivenDateAndCurrentDate();
         //dateService.convertLocalDateToUtilDate();
     }
 }
